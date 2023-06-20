c154daf732d99d180413575236c79ed8d73dcfca
de_la_verge <verge.f@gmail.com>
2023-06-20 19:01:49 +0200

    correctif main02.c

---
+++ c_03/x_mains/main_02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strcat(char *dest, char *src);
+
+int	main(void)
+{
+	char s1[50] = "Salut ";
+	char s2[] = "bande de salope";
+	
+	char s3[50] = "Salut ";
+	char s4[] = "bande de salope";
+	
+	printf("Real strcat = %s\n", strcat(s1, s2));
+	printf("ft_strcat = %s\n", ft_strcat(s3, s4));
+}
