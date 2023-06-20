a4775ecb0cdf3431ff8dafae4eb34bc749424449
de_la_verge <verge.f@gmail.com>
2023-06-20 19:01:27 +0200

    c03ex03

---
+++ c_03/ex03/ft_strncat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fverge <fverge@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/20 18:50:32 by fverge            #+#    #+#             */
+/*   Updated: 2023/06/20 18:54:59 by fverge           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = ft_strlen(dest);
+	while (i < nb && src[i] != '\0')
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
--- c_03/x_mains/main_03.c
+++ c_03/x_mains/main_03.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-char	*ft_strcat(char *dest, char *src);
+char	*ft_strncat(char *dest, char *src, unsigned int nb);
 
 int	main(void)
 {
 	char s1[50] = "Salut ";
 	char s2[] = "bande de salope";
+	unsigned int n1 = 7;
 	
 	char s3[50] = "Salut ";
 	char s4[] = "bande de salope";
+	unsigned int n2 = 7;
 	
-	printf("Real strcat = %s\n", strcat(s1, s2));
-	printf("ft_strcat = %s\n", ft_strcat(s3, s4));
+	printf("Real strncat = %s\n", strncat(s1, s2, n1));
+	printf("ft_strncat = %s\n", ft_strncat(s3, s4, n2));
 }
