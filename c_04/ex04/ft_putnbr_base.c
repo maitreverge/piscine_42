28bdf069a10a9eb3eca2b6f95b8d2aa522ff6a16
de_la_verge <verge.f@gmail.com>
2023-06-22 13:50:59 +0200

    ex04 v1

---
+++ c_04/ex04/ft_putnbr_base.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fverge <fverge@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/22 11:33:31 by fverge            #+#    #+#             */
+/*   Updated: 2023/06/22 13:50:45 by fverge           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	error_check (char *base)
+{
+	int	i;
+	int	j;
+
+	if (ft_strlen(base) < 2 || base[0] == '\0')
+		return (1);
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (base[i] == '+' || base[i] == '-')
+			return (1);
+		j = i + 1;
+		while (base[j] != '\0')
+		{
+			if (base[i] == base[j])
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	if (error_check(base) == 0)
+	{
+		if (nbr < 0)
+		{
+			ft_putchar('-');
+			nbr = nbr * (-1);
+		}
+	ft_putchar(ft_putstr(base) / 	
+	}
+}
