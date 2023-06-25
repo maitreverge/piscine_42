8674bfe9b535b8e9dbfb1a3bc92364548e6faf0f
de_la_verge <verge.f@gmail.com>
2023-06-25 18:05:54 +0200

    c06ex00

---
+++ c_06/ex00/ft_print_program_name.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fverge <fverge@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 17:59:01 by fverge            #+#    #+#             */
+/*   Updated: 2023/06/25 18:03:58 by fverge           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc >= 1)
+	{
+		ft_putstr(argv[0]);
+		ft_putchar('\n');
+	}
+}
