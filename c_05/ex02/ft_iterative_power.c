87b7ed17602d67111da993fe151e04faa91255d8
de_la_verge <verge.f@gmail.com>
2023-06-25 09:44:00 +0200

    c05ex02

---
+++ c_05/ex02/ft_iterative_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fverge <fverge@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 09:13:16 by fverge            #+#    #+#             */
+/*   Updated: 2023/06/25 09:18:46 by fverge           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	i;
+	int	result;
+
+	i = 1;
+	result = 1;
+	if (power < 0)
+		return (0);
+	while (i <= power)
+	{
+		result = result * nb;
+		i++;
+	}
+	return (result);
+}
---
+++ c_05/x_mains/main_02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ft_iterative_power(int nb, int power);
+
+int	main(void)
+{
+	int nb = 7;
+	int power = 7;
+	printf("%i", ft_iterative_power(nb, power));
+	return (0);
+}
