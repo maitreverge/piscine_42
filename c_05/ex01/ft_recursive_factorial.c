6d7148c29fab8932ae1541db50dd63da2f6de71f
de_la_verge <verge.f@gmail.com>
2023-06-25 09:12:46 +0200

    c05ex01

---
+++ c_05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fverge <fverge@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 08:43:44 by fverge            #+#    #+#             */
+/*   Updated: 2023/06/25 09:12:16 by fverge           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	int	result;
+
+	result = 1;
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	else
+		return (result * nb * ft_recursive_factorial(nb - 1));
+}
---
+++ c_05/x_mains/main_01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ft_recursive_factorial(int nb);
+
+int	main(void)
+{
+	int nb = 5;
+	printf("%i", ft_recursive_factorial(nb));
+	return (0);
+}
