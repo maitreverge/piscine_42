91bc7b9ca87955e79ee937e1120eab401e2c36c6
de_la_verge <verge.f@gmail.com>
2023-06-25 08:21:47 +0200

    c05ex00

---
+++ c_05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fverge <fverge@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 08:08:21 by fverge            #+#    #+#             */
+/*   Updated: 2023/06/25 08:21:11 by fverge           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+	int	i;
+
+	result = 1;
+	i = 1;
+	if (nb < 0)
+		return (0);
+	while (i <= nb)
+	{
+		result = result * i;
+		i++;
+	}
+	return (result);
+}
---
+++ c_05/x_mains/main_00.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ft_iterative_factorial(int nb);
+
+int	main(void)
+{
+	int nb = 5;
+	printf("%i", ft_iterative_factorial(nb));
+	return (0);
+}
