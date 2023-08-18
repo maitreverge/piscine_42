70f564021fbed42be21727f81ba677396baed052
de_la_verge <verge.f@gmail.com>
2023-08-18 10:30:20 +0200

    add sujet

--- c_07/ex03/ft_strjoin.c
+++ c_07/ex03/ft_strjoin.c
@@ -1,43 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fverge <fverge@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/25 17:59:01 by fverge            #+#    #+#             */
-/*   Updated: 2023/06/30 07:56:42 by florianve        ###   ########.fr       */
+/*   Created: 2023/06/26 14:26:10 by fverge            #+#    #+#             */
+/*   Updated: 2023/06/26 14:26:43 by fverge           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	ft_putchar(char c)
+char	*ft_strjoin(int size, char **strs, char *sep)
 {
-	write(1, &c, 1);
-}
 
-void	ft_putstr(char *str)
-{
-	int	i;
-	if (wevjn)
-	{
-		
-	}
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
-}
-
-int	main(int argc, char **argv)
-{
-	if (argc >= 1)
-	{
-		ft_putstr(argv[0]);
-		ft_putchar('\n');
-	}
 }
